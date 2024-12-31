5e8ef0e1e3f044505b3b0b1a5ec729bafe6a3e01
Subhranil Das <subhranil@itobuz.com>
2024-12-31 17:40:10 +0530

    buttons functionality added

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const todoInput = document.getElementById('todoInput');
 const todoAddBtn = document.getElementById('todoAddBtn');
 const resultSection = document.getElementById('resultSection')
+const allBtn = document.getElementById('allBtn')
+const activeBtn = document.getElementById('activeBtn')
+const completeBtn = document.getElementById('completeBtn')
+const clearBtn = document.getElementById('clearBtn')
 
 const todoList = [];
 const completeTodo = [];
 
+//Enter to submit
 todoInput.addEventListener('keypress', (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
@@ -12,6 +17,7 @@ todoInput.addEventListener('keypress', (e) => {
   }
 })
 
+//creating todo cards function
 const createTodoCardHandler = (todoArr) => {
   resultSection.innerHTML = '';
   todoArr.map((value, index) => {
@@ -28,7 +34,8 @@ const createTodoCardHandler = (todoArr) => {
 
     divContent.textContent = value;
     divContent.setAttribute('id', 'divText');
-    const divText = document.getElementById('divText');
+    divContent.setAttribute('class', 'm-0');
+    divContent.setAttribute('class', 'div-text-content');
 
     if (completeTodo.includes(value))
       divContent.setAttribute('class', 'complete-todo-content')
@@ -53,26 +60,77 @@ const createTodoCardHandler = (todoArr) => {
   })
 }
 
+//add todo eventlistener
 todoAddBtn.addEventListener('click', () => {
-  const todo = todoInput.value;
+  const todo = todoInput.value.trim();
+  if (todo !== '' && !todoList.includes(todo)) {
+    todoList.unshift(todo)
+    createTodoCardHandler(todoList);
+  }
+
+  else if (todoList.includes(todo)) {
+    alert('Todo Already Exists')
+  }
+
   todoInput.value = '';
-  todoList.unshift(todo)
-  createTodoCardHandler(todoList);
-  console.log(todoList)
 })
 
+//complete checking line through function
 function checkHandler(index) {
-  console.log("check index", index)
-  if (completeTodo.includes(todoList[index]))
+  if (completeTodo.includes(todoList[index])) {
+    completeTodo.splice(completeTodo.indexOf(todoList[index]), 1)
+  }
+
+  else {
     completeTodo.unshift(todoList[index])
-  else
-    completeTodo.splice(completeTodo.indexOf(todoList[index], 1))
-
-  divText[index].classList.toggle('complete-todo-content')
+  }
+  
+  resultSection.children[index].children[0].classList.toggle('complete-todo-content')
 }
 
+//delete todo card function
 function deleteHandler(index) {
-  console.log('delete index', index)
-  todoList.splice(index, 1);
-  createTodoCardHandler(todoList)
+  let confirmDel = confirm('Are you sure you want to delete?')
+
+  if (confirmDel) {
+    todoList.splice(index, 1);
+    createTodoCardHandler(todoList)
+  }
 }
+
+//view all todo event listener
+allBtn.addEventListener('click', () => {
+  createTodoCardHandler(todoList)
+})
+
+//view only active todos event listeners
+activeBtn.addEventListener('click', () => {
+  const activeTodoList = todoList.filter((value) => completeTodo.indexOf(value) === -1)
+  createTodoCardHandler(activeTodoList)
+})
+
+//view completed todos eventlisteners 
+completeBtn.addEventListener('click', () => {
+  if (completeTodo.length === 0)
+    resultSection.innerHTML = 'No Completed Todos to display'
+  else
+    createTodoCardHandler(completeTodo)
+})
+
+//clear all completed todos event listeners
+clearBtn.addEventListener('click', () => {
+  console.log('active todos--->', todoList)
+  console.log('complete todo--->', completeTodo)
+
+  for (let i = 0; i < todoList.length; i++) {
+    if(todoList.includes(completeTodo[i])){
+      todoList.splice(todoList.indexOf(completeTodo[i]), 1)
+      console.log('after splice--->', todoList)
+    }
+  }
+  createTodoCardHandler(todoList);
+  completeTodo.splice(0, completeTodo.length)
+
+  console.log('active todos--->', todoList)
+  console.log('complete todo--->', completeTodo)
+})
